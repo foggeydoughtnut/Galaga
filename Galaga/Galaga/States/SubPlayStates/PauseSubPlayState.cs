8953305be754961149303d1e3b40f34ea1097478
Jeff Anderson <foggeydoughtnut@gmail.com>
2023-03-28 21:01:05 -0600

    pause uses render targets now

--- Galaga/Galaga/States/SubPlayStates/PauseSubPlayState.cs
+++ Galaga/Galaga/States/SubPlayStates/PauseSubPlayState.cs
@@ -12,8 +12,12 @@ public class PauseSubPlayState : SubPlayState
     private int _indexOfChoice;
     private readonly List<string> _options;
     private readonly HighScoreTracker _tracker;
+    private readonly GraphicsDeviceManager Graphics;
+    private readonly GameWindow Window;
+    private RenderTarget2D renderTarget;
 
-    public PauseSubPlayState()
+
+    public PauseSubPlayState(GraphicsDeviceManager graphics, GameWindow window)
     {
         _options = new List<string>
         {
@@ -21,6 +25,19 @@ public class PauseSubPlayState : SubPlayState
             "Quit"
         };
         _tracker = HighScoreTracker.GetTracker();
+
+        Graphics = graphics;
+        Window = window;
+        this.renderTarget = new RenderTarget2D(
+            Graphics.GraphicsDevice,
+            1440,
+            1080,
+            false,
+            SurfaceFormat.Color,
+            DepthFormat.None,
+            Graphics.GraphicsDevice.PresentationParameters.MultiSampleCount,
+            RenderTargetUsage.DiscardContents
+        );
     }
 
     public override PlayStates Update(GameTime gameTime)
@@ -37,19 +54,40 @@ public class PauseSubPlayState : SubPlayState
 
     public override void Render(SpriteBatch spriteBatch, Dictionary<string, SpriteFont> fonts)
     {
+        this.Graphics.GraphicsDevice.SetRenderTarget(renderTarget);
+        this.Graphics.GraphicsDevice.DepthStencilState = new DepthStencilState() { DepthBufferEnable = true };
+        this.Graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+        spriteBatch.Begin(SpriteSortMode.BackToFront, samplerState: SamplerState.PointClamp);
         // Show high score
         var font = fonts["default"];
         var bigFont = fonts["big"];
         var stringSize = font.MeasureString("Score: " + _tracker.CurrentGameScore);
         spriteBatch.DrawString(font, "Score: " + _tracker.CurrentGameScore,
-            new Vector2(Constants.BOUNDS_X - stringSize.X, 0), Color.White);
+            new Vector2(renderTarget.Width - stringSize.X, 0), Color.White);
 
         // Show options
         var optionFont = _indexOfChoice == 0 ? bigFont : font;
         stringSize = optionFont.MeasureString(_options[0]);
-        RenderUtilities.CreateBorderOnWord(spriteBatch, optionFont, _options[0], new Vector2(Convert.ToInt32(Constants.BOUNDS_X / 2) - stringSize.X / 2, Convert.ToInt32(Constants.BOUNDS_Y / 2) - stringSize.Y));
+        RenderUtilities.CreateBorderOnWord(spriteBatch, optionFont, _options[0], new Vector2(Convert.ToInt32(renderTarget.Width / 2) - stringSize.X / 2, Convert.ToInt32(renderTarget.Height / 2) - stringSize.Y));
         optionFont = _indexOfChoice == 1 ? bigFont : font;
         stringSize = optionFont.MeasureString(_options[1]);
-        RenderUtilities.CreateBorderOnWord(spriteBatch, optionFont, _options[1], new Vector2(Convert.ToInt32(Constants.BOUNDS_X / 2) - stringSize.X / 2, Convert.ToInt32(Constants.BOUNDS_Y / 2) + stringSize.Y));
+        RenderUtilities.CreateBorderOnWord(spriteBatch, optionFont, _options[1], new Vector2(Convert.ToInt32(renderTarget.Width / 2) - stringSize.X / 2, Convert.ToInt32(renderTarget.Height / 2) + stringSize.Y));
+
+        spriteBatch.End();
+        Graphics.GraphicsDevice.SetRenderTarget(null);
+
+        // Render render target to screen
+        spriteBatch.Begin(SpriteSortMode.BackToFront, samplerState: SamplerState.PointClamp);
+        spriteBatch.Draw(
+                renderTarget,
+                new Rectangle(Window.ClientBounds.Width / 8, 0, (Window.ClientBounds.Height / 3 * 4), Window.ClientBounds.Height),
+                null,
+                Color.White,
+                0,
+                Vector2.Zero,
+                SpriteEffects.None,
+                1f
+            );
+        spriteBatch.End();
     }
 }
