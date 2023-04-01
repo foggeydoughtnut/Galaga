20665951d7c075a76a97b79c39111dadf375752b
Jeff Anderson <foggeydoughtnut@gmail.com>
2023-04-01 15:46:05 -0600

    smarter collision system done

--- Galaga/Galaga/Systems/CollisionDetectionSystem.cs
+++ Galaga/Galaga/Systems/CollisionDetectionSystem.cs
@@ -12,7 +12,9 @@ public class CollisionDetectionSystem : System
     private readonly PlayerSystem _playerSystem;
     private readonly EnemySystem _enemySystem;
     private readonly BulletSystem _bulletSystem;
-    public List<Tuple<Guid, string>> _collisions { get; private set; } // List of tuples that keeps track of which objects had collisions
+    //public List<Tuple<Guid, string>> _collisions { get; private set; } // List of tuples that keeps track of which objects had collisions
+    private Dictionary<Guid, string> _collisions; // List of tuples that keeps track of which objects had collisions
+
     private List<Tuple<Guid, Rectangle, string>> gameObjects;
     
     public CollisionDetectionSystem(PlayerSystem playerSystem, EnemySystem enemySystem, BulletSystem bulletSystem)
@@ -39,41 +41,39 @@ public class CollisionDetectionSystem : System
             gameObjects.Add(new (bullets[i].Id, bullets[i].Collider, "bullet"));
         }
 
-        // TODO: Add enemies to the collisions too
+        // TODO: Add enemies to the gameObjects too
 
 
         for (int i = 0; i < gameObjects.Count; i++)
         {
+            if (_collisions.ContainsKey(gameObjects[i].Item1)) continue;
             for (int j = 0; j < gameObjects.Count; j++)
             {
                 if (gameObjects[i].Item1 == gameObjects[j].Item1) continue;
-                // Check for each object, do they collide with the next object
+                if (_collisions.ContainsKey(gameObjects[j].Item1)) continue;
                 bool intersects = gameObjects[i].Item2.Intersects(gameObjects[j].Item2);
                 if (intersects)
                 {
                     // If they do, mark that both of add to the collisions dictionary and mark that both collided
-                    _collisions.Add(new(gameObjects[i].Item1, gameObjects[i].Item3));
-                    _collisions.Add(new(gameObjects[j].Item1, gameObjects[j].Item3));
-
+                    _collisions.Add(gameObjects[i].Item1, gameObjects[i].Item3);
+                    _collisions.Add(gameObjects[j].Item1, gameObjects[j].Item3);
                 }
             }
         }
 
         // Go through each collision and notify the system that it had a collision
-        for (int i = 0; i < _collisions.Count; i++)
+        foreach (KeyValuePair<Guid, string> collision in _collisions)
         {
-            Guid id = _collisions[i].Item1;
-            string type = _collisions[i].Item2;
 
-            if (type == "player")
+            if (collision.Value == "player")
             {
                 _playerSystem.PlayerHit();
             }
-            else if (type == "bullet")
+            else if (collision.Value == "bullet")
             {
-                _bulletSystem.ObjectHit(id);
+                _bulletSystem.ObjectHit(collision.Key);
             }
-            else if (type == "enemy")
+            else if (collision.Value == "enemy")
             {
                 //TODO notify enemy that it was hit
             }
