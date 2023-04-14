14a712d29176bfb34ce3643faa7a895e45881e68
Jeff Anderson <foggeydoughtnut@gmail.com>
2023-04-13 19:51:00 -0600

    can select controls to rebind them, will need to actually use those controls in the game now

--- Galaga/Galaga/States/ControlsState.cs
+++ Galaga/Galaga/States/ControlsState.cs
@@ -5,29 +5,49 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Galaga.States
 {
+
+
     public class ControlsState : GameState
     {
         RenderTarget2D renderTarget;
         private int _indexOfChoice;
         private List<string> _controls;
         private bool _listening;
+        KeyboardState previousKeyboardState;
+        Keys pressedKey;
+        Controls controls;
+
 
 
         public override void Initialize(GraphicsDevice graphicsDevice, GraphicsDeviceManager graphics, GameWindow window)
         {
+            if (!File.Exists("controls.json"))
+            {
+                Controls newControls = new Controls {Left = Keys.Left, Right = Keys.Right, Fire = Keys.Space };
+                string json = JsonConvert.SerializeObject(newControls);
+                File.WriteAllText("controls.json", json);
+            }
+
+            string controlsFileData = File.ReadAllText("controls.json");
+            controls = JsonConvert.DeserializeObject<Controls>(controlsFileData);
+
+
+            // Todo read controls from json file
             _listening = false;
             _controls = new()
             {
-                $"Move Left: {Keys.Left}",
-                $"Move Right: {Keys.Right}",
-                $"Fire: {Keys.Space}"
+                $"Move Left: {controls.Left}",
+                $"Move Right: {controls.Right}",
+                $"Fire: {controls.Fire}"
             };
             this.renderTarget = new RenderTarget2D(
                 graphicsDevice,
@@ -39,6 +59,8 @@ namespace Galaga.States
                 graphicsDevice.PresentationParameters.MultiSampleCount,
                 RenderTargetUsage.DiscardContents
             );
+            previousKeyboardState = Keyboard.GetState();
+            pressedKey = new();
             base.Initialize(graphicsDevice, graphics, window);
         }
         public override void LoadContent(ContentManager contentManager)
@@ -48,35 +70,62 @@ namespace Galaga.States
             Fonts.Add("veryBig", contentManager.Load<SpriteFont>("Fonts/DemoFont3"));
         }
 
-        // Example of keyReleased event
-        private bool keyReleased(Keys key)
-        {
-            return (Keyboard.GetState().IsKeyUp(key) && true);// state previous was down)
-        }
 
         protected override void ProcessInput()
         {
+            KeyboardState currentKeyboardState = Keyboard.GetState();
             if (!_listening)
             {
-                if (IsKeyPressed(Keys.Up) && _indexOfChoice - 1 >= 0)
+                if (currentKeyboardState.IsKeyUp(Keys.Up) && previousKeyboardState.IsKeyDown(Keys.Up) && _indexOfChoice - 1 >= 0)
                     _indexOfChoice -= 1;
 
-                if (IsKeyPressed(Keys.Down) && _indexOfChoice + 1 < _controls.Count)
+                if (currentKeyboardState.IsKeyUp(Keys.Down) && previousKeyboardState.IsKeyDown(Keys.Down) && _indexOfChoice + 1 < _controls.Count)
                     _indexOfChoice += 1;
 
-                if (IsKeyPressed(Keys.Enter))
+                if (currentKeyboardState.IsKeyUp(Keys.Enter) && previousKeyboardState.IsKeyDown(Keys.Enter))
                 {
                     _listening = true;
                 }
             }
-            else
+            else // listening
             {
-                // listening
-            }
-            // TODO Take what the user inputs after and then put that into a variable.
-            // Write that value to be the new input for the specific command
-            // Write that new data to the file
+                foreach (Keys key in currentKeyboardState.GetPressedKeys())
+                {
+                    // Take what the user inputs after and then put that into a variable.
+                    pressedKey = key;
+                    break;
+                }
+                //Debug.WriteLine(pressedKey);
+                if (pressedKey != Keys.None)
+                {
+                    if (_indexOfChoice == 0) // Left
+                    {
+                        controls.Left = pressedKey;
+                        _controls[0] = $"Move Left: {controls.Left}";
+                    }
+                    else if (_indexOfChoice == 1) // Right
+                    {
+                        controls.Right = pressedKey;
+                        _controls[1] = $"Move Right: {controls.Right}";
+                    }
+                    else if (_indexOfChoice == 2) // Fire
+                    {
+                        controls.Fire = pressedKey;
+                        _controls[2] = $"Fire: {controls.Fire}";
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Invalid index");
+                    }
+                    pressedKey = Keys.None;
+                    _listening = false;
 
+                    // Write that value to be the new input for the specific command
+                    string json = JsonConvert.SerializeObject(controls);
+                    File.WriteAllText("controls.json", json);
+                }
+            }
+            previousKeyboardState = currentKeyboardState;
             base.ProcessInput();
 
         }
@@ -94,6 +143,12 @@ namespace Galaga.States
                 new Vector2(Convert.ToInt32(renderTarget.Width / 2) - titleSize.X / 2, Convert.ToInt32(renderTarget.Height / 4)));
 
             SpriteFont font = Fonts["default"];
+            if (_listening)
+            {
+                string message = "Listening...";
+                Vector2 stringSize = font.MeasureString(message);
+                RenderUtilities.CreateBorderOnWord(SpriteBatch, font, message, new Vector2(Convert.ToInt32(renderTarget.Width / 2) - stringSize.X / 2, Convert.ToInt32(renderTarget.Height / 2 - stringSize.Y)));
+            }
             for (int i = 0; i < _controls.Count; i++)
             {
                 SpriteFont optionFont = _indexOfChoice == i ? bigFont : font;
---
+++ Galaga/Galaga/Utilities/Controls.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaga.Utilities
+{
+    public class Controls
+    {
+        public Keys Left { get; set; }
+        public Keys Right { get; set; }
+        public Keys Fire { get; set; }
+    }
+}
